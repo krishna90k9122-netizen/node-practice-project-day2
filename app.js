08508f31d256c883dd91c3948d16ecce22e85cf9
Krishna Gupta <krishna90k9122@mail.com>
2026-06-17 09:28:23 +0530

    ejs

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const path= require('path');
 
 const app=express();
 
+app.use(express.static("public"));  
 app.use(express.urlencoded({extended:true}));
 
+//ejs setup 
+app.set("view engine" ,"ejs");
+
+app.get("/ejs",(req,resp)=>{
+    resp.render("ejs-demo",{
+    name:"Krishna"
+    });
+});
+
 app.get("/",(req,resp)=>{
-    resp.sendFile(path.join(__dirname,"views","home.html"));
+    resp.sendFile(path.join(__dirname,"views","login.html"));
 });
 
 app.post("/submit",(req,resp)=>{
